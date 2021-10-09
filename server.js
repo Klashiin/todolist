e1580310eab451cda8409c83dcd5844028369b9c
Nicollas <fagundesnicollas@gmail.com>
2021-10-09 18:36:29 -0300

    copiei do vídeo russo o deploy

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); 
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200;
+const router = jsonServer.router('db/db.json'); 
+const middlewares = jsonServer.defaults({
+    static: './build'
+});
+const port = process.env.PORT || 8000;
 
 server.use(middlewares);
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+}));
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => console.log('Server is running!'));
