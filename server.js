d4e39fea1688d7867dee96221aff8f6ec72f16f4
Nicollas <fagundesnicollas@gmail.com>
2021-10-09 18:40:59 -0300

    change ./build to dist on server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db/db.json'); 
 const middlewares = jsonServer.defaults({
-    static: './build'
+    static: 'dist'
 });
 const port = process.env.PORT || 8000;
 
